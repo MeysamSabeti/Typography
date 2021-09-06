f0b54379acfaa04cb77633143c867e7b431c0a70
meysam sabeti <meysam_sabeti8@yahoo.com>
2021-09-06 15:02:00 +0430

    set shadow and color

--- script.js
+++ script.js
@@ -6,8 +6,22 @@ hero.addEventListener('mousemove', function (event) {
     const { offsetWidth: width , offsetHeight: height } = hero
     let {offsetX:x , offsetY:y } = event
 
-    y+=event.target.offsetLeft;
-    x+= event.target.offsetTop;
+    x+=event.target.offsetLeft;
+    y+= event.target.offsetTop;
+
+    const yWalk = Math.round((x / width * walk ) - (walk / 2))
+    const xWalk = Math.round((y / height * walk ) - (walk / 2))
+    console.log(xWalk , yWalk);
+
+    text.style.textShadow = `
+    ${xWalk}px ${yWalk}px 0 rgba(238,82,83,.7),
+    ${yWalk}px ${xWalk}px 0 rgba(52,31,151,.7),
+    ${xWalk * -1}px ${yWalk *-1}px 0 rgba(143,212,10,.7),
+    ${xWalk}px ${yWalk *-1}px 0 rgba(18,22,400,.7),
+    ${yWalk *-1}px ${xWalk *-1}px 0 rgba(86,143,25,.7),
+    ${xWalk * -1}px ${yWalk}px 0 rgba(102,17,186,.7),
+    ${yWalk}px ${xWalk * -1}px 0 rgba(243,104,224,.7),
+    ${yWalk * -1}px ${xWalk}px 0 rgba(254,202,87,.7)`
 
 
 })
